84ac925ae3806c0d2ff00d514eeecd4f49754f95
AilysGeister <samantha.guivret@gmail.com>
2026-01-21 01:47:54 +0100

    init .env 2

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-use Dotenv\Dotenv;
-
-//Setup the environ:
 require 'vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
+//Setup the environment:
 $env = Dotenv::createImmutable(__DIR__);
 $env->load();
 ?>
+<h1>hello world</h1>
