9d1b92f1af100d274b65c9af6e10db45d2168a86
AilysGeister <samantha.guivret@gmail.com>
2026-02-04 14:07:43 +0100

    transfer for db connection

---
+++ src/controller/db_MySQL.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/DataBase.php';
+
+/**
+ * Work with a My SQL database.
+ */
+class db_MySQL extends Database {
+
+    private $connection;
+     public function __construct() {
+         $this->connection = new mysqli(
+             $_ENV['DB_HOST'],
+             $_ENV['DB_USERNAME'],
+             $_ENV['DB_PASSWORD'],
+             $_ENV['DB_NAME'],
+             $_ENV['DB_PORT']);
+     }
+
+    public function query($query): bool | mysqli_result {
+        return mysqli_query($this->connection, $query);
+    }
+}
